34935ddf651139f4a06ea032e000be983e01a3dd
JosueGarcia04 <josueadrian042006@gmail.com>
2025-10-18 15:39:12 -0600

    config upload preset de cloudinary

---
+++ src/hooks/useImageUpload.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Resizer from "react-image-file-resizer";
+import axios from "axios";
+
+const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD;
+const UPLOAD_PRESET = "gastrobar_posts";
+
+export default function useImageUpload() {
+  const [loading, setLoading] = useState(false);
+
+  const subir = (file) =>
+    new Promise((resolve, reject) => {
+      setLoading(true);
+      Resizer.imageFileResizer(
+        file,
+        1280, 720, "JPEG", 85, 0,
+        (uri) => {
+          const fd = new FormData();
+          fd.append("file", uri);
+          fd.append("upload_preset", UPLOAD_PRESET);
+          axios
+            .post(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`, fd)
+            .then(res => {
+              setLoading(false);
+              resolve(res.data.secure_url);
+            })
+            .catch(err => {
+              setLoading(false);
+              reject(err);
+            });
+        },
+        "blob"
+      );
+    });
+
+  return { subir, loading };
+}
