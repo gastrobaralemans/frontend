064a23eadcf3c8dbf765152d5ecb97238b89a83c
JosueGarcia04 <josueadrian042006@gmail.com>
2025-10-18 19:36:14 -0600

    store modificado

--- src/store/forgotpasswordstore.js
+++ src/store/forgotpasswordstore.js
@@ -3,14 +3,14 @@ import { persist } from 'zustand/middleware';
 
 export const useForgotPasswordStore = create(
   persist(
-    (set, get) => ({
+    (set) => ({
       email: '',
       code: '',
       step: 'codeMail',
       setMail: (email) => set({ email }),
       setCode: (code) => set({ code }),
       setStep: (step) => set({ step }),
-      reset: () => set({ email: '', code: '', step: 'codeMail' })
+      reset: () => set({ email: '', code: '', step: 'codeMail' }),
     }),
     {
       name: 'forgot-password-storage',
